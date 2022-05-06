270ccbe3ae036c3dc61b84709c069f90111af2c5
Marcelo Soares <marcelo@mmda.com.br>
2022-05-06 19:50:45 -0300

    started and stuff, now we have the definitions

---
+++ pinger.py
@@ -0,0 +1,37 @@
+'''
+Escreva um programa em Python que:
+- Recebe uma URL digitada via teclado
+- Realiza um ping da respectiva URL
+- Retorna o ip (se houver) da URL digitada
+- Considere o exemplo de execução apresentado abaixo:
+
+
+Critérios de Avaliação:
+- Conceito C
+- Entrada:
+- URL’s para serem pingadas devem entrar via teclado
+- Saída:
+- Resultado de cada ping mostrado na tela, conforme exemplo acima
+- Ao final do execução de cada ping, o programa deve mostrar na tela uma Tabela de Resultados
+- A Tabela de Resultados deve conter os campos: URL e IP, formatados da seguinte
+maneira (veja imagem abaixo e/ou acima):
+
+- Conceito B
+- Entrada:
+- URL’s para serem pingadas devem entrar via teclado
+- Saída:
+- Resultado de cada ping mostrado na tela, conforme exemplo acima.
+- Ao final do execução do programa, isto é, quando o usuário indicar que não deseja continuar,
+o programa deve mostrar na tela uma única Tabela de Resultados contendo os dados de todos
+os pings realizado durante a execução.
+- A Tabela de Resultados deve conter os campos:
+URL, IP, Status (Ativo ou Inativo), Data e Hora, formatados da seguinte maneira
+(veja imagem abaixo):
+
+- Conceito A, indo além:
+- Entrada:
+- URL’s para serem pingadas a partir de um arquivo .txt
+
+- Os pings devem ser disparados automaticamente (uma URL por vez) e uma mensagem deve
+ser informada na tela para o usuário:
+'''
